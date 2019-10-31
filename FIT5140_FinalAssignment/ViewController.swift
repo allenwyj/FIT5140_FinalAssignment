94efff71774d94d965fd79ecd961b5f7bdc27fc6
Yujie wU <ywu3-6>
2019-10-31 13:07:44 +1100

    Initial Commit

---
+++ FIT5140_FinalAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT5140_FinalAssignment
+//
+//  Created by Yujie Wu on 31/10/19.
+//  Copyright © 2019 Yujie Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
